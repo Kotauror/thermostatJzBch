4bc9d8d61fe678527c1162e7e2c3ab7396f61368
kotpsot <zygmunt.justyna@gmail.com>
2018-02-21 15:03:13 +0000

    Add mode switch function.

--- jasmine-standalone-2.3.4/spec/ThermostatSpec.js
+++ jasmine-standalone-2.3.4/spec/ThermostatSpec.js
@@ -13,6 +13,11 @@ describe("Thermostat", function() {
     expect(thermostat.mode).toEqual("save");
   })
 
+  it("it can change a mode", function() {
+    thermostat.changeMode();
+    expect(thermostat.mode).toEqual("normal");
+  })
+
   it("it can increase temperature", function() {
     thermostat.increase(5);
     expect(thermostat.temperature).toEqual(25);
@@ -21,6 +26,11 @@ describe("Thermostat", function() {
   it("it can decrease temperature", function() {
     thermostat.decrease(5);
     expect(thermostat.temperature).toEqual(15);
+  })
+
+  it("temperature cannot be less than 10 deg", function() {
+    thermostat.decrease(15);
+    expect(thermostat.temperature).toEqual(10);
   })
 
 })
--- jasmine-standalone-2.3.4/src/Thermostat.js
+++ jasmine-standalone-2.3.4/src/Thermostat.js
@@ -3,10 +3,18 @@ function Thermostat(temperature = 20, mode = "save") {
   this.mode = mode;
 };
 
+Thermostat.prototype.changeMode = function() {
+  this.mode = "normal";
+}
+
 Thermostat.prototype.increase = function(number) {
   this.temperature += number;
 }
 
 Thermostat.prototype.decrease = function(number) {
-  this.temperature -= number;
+  if (this.temperature - number < 10) {
+    this.temperature = 10;
+  } else {
+    this.temperature -= number;
+  }
 }
