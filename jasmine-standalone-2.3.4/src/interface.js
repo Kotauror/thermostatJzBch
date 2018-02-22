def3478de390a22c93d6af3742db2db52e5819c2
kotpsot <zygmunt.justyna@gmail.com>
2018-02-22 20:02:32 +0000

    Thermostat done, setup for api

--- jasmine-standalone-2.3.4/src/interface.js
+++ jasmine-standalone-2.3.4/src/interface.js
@@ -23,25 +23,17 @@ $( document ).ready(function() {
   $('.increase_form').submit(function(e) {
     console.log(e)
     e.preventDefault();
-    // console.log(e.target.input.value)
     thermostat.increase(parseInt(e.target.temperature.value))
     updateTemperature()
     thermostat.maxTemperatureAlert()
   })
 
   $('.decrease_form').submit(function(e) {
-    // console.log(e)
     e.preventDefault();
-    // console.log(e.target.input.value)
     thermostat.decrease(parseInt(e.target.temperature.value))
     updateTemperature()
     thermostat.minTemperatureAlert()
   })
-
-  $( "#target" ).submit(function( event ) {
-    alert( "Handler for .submit() called." );
-    event.preventDefault();
-  });
 
   function updateTemperature() {
     $('#number').text(thermostat.temperature);
