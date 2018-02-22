d1381a91c5cc6272fb94d4f5d0ed702067b0b55a
kotpsot <zygmunt.justyna@gmail.com>
2018-02-22 19:37:04 +0000

    improve css

--- jasmine-standalone-2.3.4/src/interface.js
+++ jasmine-standalone-2.3.4/src/interface.js
@@ -9,21 +9,22 @@ $( document ).ready(function() {
 
   $('#usageinfo').text(thermostat.usage());
 
-  $('#switch_button').click(function(e) {
+  $('#switch').click(function(e) {
   thermostat.changeMode();
   updateMode();
   });
 
-  $('#reset_button').click(function() {
+  $('#reset').click(function() {
   thermostat.reset();
   updateTemperature();
   updateMode();
   });
 
   $('.increase_form').submit(function(e) {
+    console.log(e)
     e.preventDefault();
     // console.log(e.target.input.value)
-    thermostat.increase(parseInt(e.target.input.value))
+    thermostat.increase(parseInt(e.target.temperature.value))
     updateTemperature()
     thermostat.maxTemperatureAlert()
   })
@@ -32,7 +33,7 @@ $( document ).ready(function() {
     // console.log(e)
     e.preventDefault();
     // console.log(e.target.input.value)
-    thermostat.decrease(parseInt(e.target.input.value))
+    thermostat.decrease(parseInt(e.target.temperature.value))
     updateTemperature()
     thermostat.minTemperatureAlert()
   })
@@ -44,7 +45,7 @@ $( document ).ready(function() {
 
   function updateTemperature() {
     $('#number').text(thermostat.temperature);
-    $('#usage_color').attr('class', thermostat.usage());
+    $('#temperature').attr('class', thermostat.usage());
   }
 
   function updateMode() {
